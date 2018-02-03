db57fcee02d83ecc7d2a812a0813bb924f5b73ed
Jena Dorrin <JRose81688@gmail.com>
2018-02-02 21:25:34 -0500

    java exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (add1, add2) {
+	return add1 + add2;
 }
 
-function subtract () {
-	
+function subtract (sub1, sub2) {
+	return sub1 - sub2;
 }
 
-function sum () {
-	
+function sum (inputs) {
+	let sum = 0;
+	for(i = 0; i < inputs.length; i++){
+		sum += inputs[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (elements) {
+let product = 1;
+for(i = 0; i < elements.length; i++){
+	product *= elements[i];
+}
+return product;
 }
 
-function power() {
-	
+function power(pow1, pow2) {
+	return pow1 ** pow2;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let factorial = 1;
+	for(i = num; i > 0; i--){
+			factorial *= i;
+		}
+return factorial;
 }
 
 module.exports = {
