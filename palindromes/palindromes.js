8da2b95ede9c207c3ea0c1f73586c0d7add3644b
Jena Dorrin <JRose81688@gmail.com>
2018-02-03 22:51:46 -0500

    completed more exercises

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,14 @@
-var palindromes = function() {
+var palindromes = function(palindromeInput) {
+var palindromeNoPunctuation = palindromeInput.toLowerCase().replace("!.,", "");
+var palindomeNoSpaces = palindromeNoPunctuation.split(" ").join("");
+var reversePalindrome = palindromeNoPunctuation.split(" ").reverse().join("");
+
+if(palindomeNoSpaces == reversePalindrome){
+  return true;
+} else {
+  return false;
+}
+
 
 }
 
--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,21 +1,22 @@
-function echo() {
-	
+function echo(input) {
+	return input;
 }
 
-function shout() {
-	
+function shout(inputStr) {
+	return inputStr.toUpperCase();
 }
 
-function repeat() {
-
+function repeat(inputRepeat, inputVar = 2) {
+	return (inputRepeat + ' ').repeat(inputVar).trim();
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(str, indx) {
+	return str.substring(0, indx);
 }
 
-function firstWord() {
-	
+function firstWord(firstWordString) {
+	let firstWordResult = firstWordString.split(' ')
+ return firstWordResult[0];
 }
 
 function capitalize(word) {
@@ -23,8 +24,33 @@ function capitalize(word) {
 	// This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
+// function titleCreator(titleCreatorString) {
+// let result1 = titleCreatorString.split(' ');
+// 	for(i = 0; i < result1.length; i++){
+// 		result1[i] = result1[i].charAt(0).toUpperCase() + result1[i].substr(1);
+// 		if(i != 0 && (result1[i]=="And" || result1[i] =="The" || result1[i]=="Over")){
+// 			result1[i] = result1[i].toLowerCase();
+// 		}
+// 	}
+// str = result1.join(" ");
+// // str1 = str.charAt(0).toUpperCase() + str.substr(1);
+//
+//
+// return str;
+//
+// }
+
+function titleCreator(titleCreatorString) {
+	let smallWords = ['and', 'the', 'over'];
+	let words = titleCreatorString.split(' ');
+
+	for(i = 0; i < words.length; i++) {
+		if (i > 0 && smallWords.includes(words[i])) {
+			continue;
+		}
+		words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+	}
+	return words.join(' ');
 }
 
 module.exports = {
